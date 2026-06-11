0e880337aced6df13c241065ea29e61e268a3f8f
martogasha <m.gachanga97@gmail.com>
2026-06-11 15:54:08 +0300

    firstvsdvas

--- resources/views/admin/mpesaReceipt.blade.php
+++ resources/views/admin/mpesaReceipt.blade.php
@@ -9,13 +9,7 @@
 
 <div class="page-content container">
     <div class="page-header text-blue-d2">
-        <h1 class="page-title text-secondary-d1">
-            Receipt
-            <small class="page-info">
-                <i class="fa fa-angle-double-right text-80"></i>
-                ID: 0{{$receipt->id}}
-            </small>
-        </h1>
+       
 
         <div class="page-tools">
             <div class="action-buttons">
@@ -30,280 +24,115 @@
             </div>
         </div>
     </div>
-
+<br>
     <div class="container px-0" id="receipt">
-        <div class="row mt-4" id="rrr">
-            <div class="col-sm-6">
-                <div>
-                    <span class="text-sm text-grey-m2 align-middle"><img src="{{asset('img/jp.png')}}" alt="logo" style="width:150px;height:150px;"></span>
-                    <span class="text-600 text-110 text-blue align-middle"><br style="color:blue">Vumatel Networks Limited</span>
-                </div>
-            </div>
-            <div class="col-12 col-lg-10 offset-lg-1">
-                <!-- .row -->
-                <hr class="row brc-default-l1 mx-n1 mb-4" />
-
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div>
-                            <span class="text-sm text-grey-m2 align-middle">To:</span>
-                            @if(!is_null($user))
-                            <span class="text-600 text-110 text-blue align-middle">{{$user->first_name}} <span style="color:black;">{{$user->phone}}</span></span>
-                            @else
-                                <span class="text-600 text-110 text-blue align-middle">{{$receipt->invoice->user->first_name}} {{$receipt->invoice->user->phone}}</span>
-
-                            @endif
+  <div class="container">
+<div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="invoice-title">
+                        <span class="text-sm text-grey-m2 align-middle"><img src="{{asset('img/jp.png')}}" alt="logo" style="width:150px;height:150px;"></span>
+                        <div class="d-flex justify-content-end">
+                           <h2 class="mb-1 text-muted">RECEIPT</h2>
                         </div>
-                        <div class="text-grey-m2">
-                            <div class="my-1">
-                                @if(!is_null($user))
-                                {{$user->last_name}}
-                                @else
-                                    N/A
-                                @endif
+                     <div class="row">
+                        <div class="col-sm-6">
+                            <div class="text-muted">
+                                <h5 class="font-size-16 mb-3">Recieved From:</h5>
+                                <p class="mb-1">{{$user->first_name}}</p>
+                               
+                                <p>{{$user->phoneOne}}</p>
                             </div>
-                          
-                            @if(!is_null($user))
-                            <div class="my-1"><i class="fa fa-phone fa-flip-horizontal text-secondary"></i> <b class="text-600">{{$user->phoneOne}}</b></div>
-                            @else
-                                <div class="my-1"><i class="fa fa-phone fa-flip-horizontal text-secondary"></i> <b class="text-600">{{$receipt->senderPhoneNumber}}</b></div>
-                            @endif
                         </div>
+                        <!-- end col -->
+                  
+                        <!-- end col -->
                     </div>
-                        <!-- /.col -->
+                    </div>
 
-                    <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
-                        <hr class="d-sm-none" />
-                        <div class="text-grey-m2">
-                            <div class="mt-1 mb-2 text-secondary-m1 text-600 text-125">
-                                Receipt
+                    <hr class="my-4">
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="text-muted">
+                                <h5 class="font-size-16 mb-3">Paid To:</h5>
+                                <p class="mb-1">Paybill: 4311304</p>
+                                <p class="mb-1">Vumatel Networks</p>
+                                <p class="mb-1">Greec Towers</p>
+                                <p>Ruiru, 00233</p>
+                              
                             </div>
-
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">ID:</span> 0{{$receipt->id}}</div>
-
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Issue Date:</span> {{$receipt->originationTime}}</div>
-                                <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Payment method:</span> Mpesa</div>
-                                <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Ref No:</span> <span class="badge badge-success badge-pill px-25">{{$receipt->reference}}</span></div>
                         </div>
+                        <!-- end col -->
+                  
+                        <!-- end col -->
                     </div>
-                    <!-- /.col -->
-                </div>
+                    <!-- end row -->
+                    
+                    <div class="py-2">
 
-                <div class="mt-4">
-                    <div class="row text-600 text-white bgc-default-tp1 py-25">
-                        <div class="d-none d-sm-block col-1"></div>
-                        <div class="col-9 col-sm-5">Description</div>
-                        <div class="d-none d-sm-block col-4 col-sm-2"></div>
-                        <div class="d-none d-sm-block col-sm-2"></div>
-                        <div class="col-2">Amount</div>
-                    </div>
-                    @if(!is_null($receipt->invoice_id))
-                    <div class="text-95 text-secondary-d3">
-                        <div class="row mb-2 mb-sm-0 py-25">
-                            <div class="d-none d-sm-block col-1"></div>
-                            <div class="col-9 col-sm-5">Invoiced</div>
-                            <div class="d-none d-sm-block col-2"></div>
-                            <div class="d-none d-sm-block col-2 text-95"></div>
-                            <div class="col-2 text-secondary-d2">SH{{$receipt->invoice->amount}}</div>
-                        </div>
-                    </div>
-                    @endif
-                    <div class="text-95 text-secondary-d3">
-                        <div class="row mb-2 mb-sm-0 py-25">
-                            <div class="d-none d-sm-block col-1"></div>
-                            <div class="col-9 col-sm-5">{{$receipt->system}}</div>
-                            <div class="d-none d-sm-block col-2"></div>
-                            <div class="d-none d-sm-block col-2 text-95"></div>
-                            <div class="col-2 text-secondary-d2">SH{{$receipt->amount}}</div>
-                        </div>
-                    </div>
-
-                    <div class="row border-b-2 brc-default-l2"></div>
-
-                    <!-- or use a table instead -->
-                    <!--
-            <div class="table-responsive">
-                <table class="table table-striped table-borderless border-0 border-b-2 brc-default-l1">
-                    <thead class="bg-none bgc-default-tp1">
-                        <tr class="text-white">
-                            <th class="opacity-2">#</th>
-                            <th>Description</th>
-                            <th>Qty</th>
-                            <th>Unit Price</th>
-                            <th width="140">Amount</th>
-                        </tr>
-                    </thead>
-
-                    <tbody class="text-95 text-secondary-d3">
-                        <tr></tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Domain registration</td>
-                            <td>2</td>
-                            <td class="text-95">$10</td>
-                            <td class="text-secondary-d2">$20</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            -->
-
-                    <div class="row mt-3">
-                        <div class="col-12 col-sm-7 text-grey-d2 text-95 mt-2 mt-lg-0">
-                        </div>
-
-                        <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
-                            <div class="row my-2 align-items-center bgc-primary-l3 p-2">
-                                <div class="col-7 text-right">
-                                    Total Amount
-                                </div>
-                                <div class="col-5">
-                                    <span class="text-150 text-success-d3 opacity-2"><b>SH{{$receipt->amount}}</b></span>
-                                </div>
-                            </div>
-                            @if(!is_null($receipt->invoice_id))
-                            @if($receipt->invoice_balance>0)
-                                <div class="row my-2 align-items-center bgc-primary-l3 p-2">
-                                    <div class="col-7 text-right">
-                                        <b style="color: red">Balance</b>
-                                    </div>
-                                    <div class="col-5">
-                                        <span class="text-150 text-success-d3 opacity-2"><b style="color: red">SH{{$receipt->invoice_balance}}</b></span>
-                                    </div>
-                                </div>
-                                @endif
-                                @endif
-                        </div>
-                    </div>
-
-                    <hr/>
-
-                    <div>
-                        <span class="text-secondary-d1 text-105">We Deliver as promised</span>
+                        <div class="table-responsive">
+                            <table class="table align-middle table-nowrap table-centered mb-0">
+                                <thead>
+                                    <tr>
+                                        <th>DESCRIPTION</th>
+                                        <th>RATE</th>
+                                        <th>QUANTITY</th>
+                                        <th>AMOUNT</th>
+                                    </tr>
+                                </thead><!-- end thead -->
+                                <tbody>
+                                    <tr>
+                                          <td>
+                                            <div>
+                                                <p class="text-muted mb-0">30 - day Internet Subscription</p>
+                                                <h5 class="text-truncate font-size-14 mb-1">( {{date('jS M',strtotime($receipt->originationTime))}} to {{date('jS M',strtotime($user->due_date))}} )</h5>
+                                                
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div>
+                                                <h5 class="text-truncate font-size-14 mb-1">{{ number_format($receipt->amount, 0, '.', ',') }}</h5>
+                                            </div>
+                                        </td>
+                                        <td style="text-align:center;">1</td>
+                                         <td>
+                                            <div>
+                                                <h5 class="text-truncate font-size-14 mb-1">{{ number_format($receipt->amount, 0, '.', ',') }}</h5>
+                                            </div>
+                                        </td>
+                                        
+                                    </tr>
+                                    <!-- end tr -->
+                             
+                                    <!-- end tr -->
+                         
+                                    <!-- end tr -->
+                  
+                                    <!-- end tr -->
+                              
+                                    <!-- end tr -->
+                                     <hr>
+                                    <tr>
+                                        <th scope="row" colspan="4" class="border-0 text-end">TOTAL</th>
+                                        <td class="border-0 text-end"><h4 class="m-0 fw-semibold">{{ number_format($receipt->amount, 0, '.', ',') }}</h4></td>
+                                    </tr>
+                                    <!-- end tr -->
+                                </tbody><!-- end tbody -->
+                            </table><!-- end table -->
+                        </div><!-- end table responsive -->
+              
                     </div>
                 </div>
             </div>
-        </div>
-
+        </div><!-- end col -->
+    </div>
+</div>
     </div>
 </div>
 <style>
-    body{
-        margin-top:20px;
-        color: #484b51;
-    }
-    .text-secondary-d1 {
-        color: #728299!important;
-    }
-    .page-header {
-        margin: 0 0 1rem;
-        padding-bottom: 1rem;
-        padding-top: .5rem;
-        border-bottom: 1px dotted #e2e2e2;
-        display: -ms-flexbox;
-        display: flex;
-        -ms-flex-pack: justify;
-        justify-content: space-between;
-        -ms-flex-align: center;
-        align-items: center;
-    }
-    .page-title {
-        padding: 0;
-        margin: 0;
-        font-size: 1.75rem;
-        font-weight: 300;
-    }
-    .brc-default-l1 {
-        border-color: #dce9f0!important;
-    }
-
-    .ml-n1, .mx-n1 {
-        margin-left: -.25rem!important;
-    }
-    .mr-n1, .mx-n1 {
-        margin-right: -.25rem!important;
-    }
-    .mb-4, .my-4 {
-        margin-bottom: 1.5rem!important;
-    }
-
-    hr {
-        margin-top: 1rem;
-        margin-bottom: 1rem;
-        border: 0;
-        border-top: 1px solid rgba(0,0,0,.1);
-    }
-
-    .text-grey-m2 {
-        color: #888a8d!important;
-    }
-
-    .text-success-m2 {
-        color: #86bd68!important;
-    }
-
-    .font-bolder, .text-600 {
-        font-weight: 600!important;
-    }
-
-    .text-110 {
-        font-size: 110%!important;
-    }
-    .text-blue {
-        color: #478fcc!important;
-    }
-    .pb-25, .py-25 {
-        padding-bottom: .75rem!important;
-    }
-
-    .pt-25, .py-25 {
-        padding-top: .75rem!important;
-    }
-    .bgc-default-tp1 {
-        background-color: rgba(121,169,197,.92)!important;
-    }
-    .bgc-default-l4, .bgc-h-default-l4:hover {
-        background-color: #f3f8fa!important;
-    }
-    .page-header .page-tools {
-        -ms-flex-item-align: end;
-        align-self: flex-end;
-    }
-
-    .btn-light {
-        color: #757984;
-        background-color: #f5f6f9;
-        border-color: #dddfe4;
-    }
-    .w-2 {
-        width: 1rem;
-    }
-
-    .text-120 {
-        font-size: 120%!important;
-    }
-    .text-primary-m1 {
-        color: #4087d4!important;
-    }
-
-    .text-danger-m1 {
-        color: #dd4949!important;
-    }
-    .text-blue-m2 {
-        color: #68a3d5!important;
-    }
-    .text-150 {
-        font-size: 150%!important;
-    }
-    .text-60 {
-        font-size: 60%!important;
-    }
-    .text-grey-m1 {
-        color: #7b7d81!important;
-    }
-    .align-bottom {
-        vertical-align: bottom!important;
-    }
+   
 </style>
 <!-- jquery-->
 <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
