9bd1694f84a76e082c7234684ffe1f9023e9a2bd
martogasha <m.gachanga97@gmail.com>
2026-08-12 10:26:23 +0300

    firstvsdvdccvs

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -937,8 +937,8 @@ class MpesaController extends Controller
             $body = [
                 'ShortCode' => '4311304',
                 'OrganizationName' => "VUMATEL NETWORKS",
-                'StartDate' => '2026-08-05 00:00:00',
-                'EndDate' => '2026-08-10 23:59:59',
+                'StartDate' => '2026-08-12 00:00:00',
+                'EndDate' => '2026-08-12 09:59:59',
                 'OffSetValue' => '0'
             ];
 
